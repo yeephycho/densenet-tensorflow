854022be817be1bb46f6272e704bc703354f3527
huyixuan <huyixuan@huyixuans-MacBook-Pro.local>
2017-08-24 22:17:31 +0800

    update trainer

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
-# Brief: Train a densenet for image classification
-# Data: 24/Aug./2017
-# E-mail: huyixuanhyx@gmail.com
-# License: Apache 2.0
-# By: Yeephycho @ Hong Kong
+# Brief:     Train a densenet for image classification
+# Data:      24/Aug./2017
+# E-mail:    huyixuanhyx@gmail.com
+# License:   Apache 2.0
+# By:        Yeephycho @ Hong Kong
 
 # Code still under construction
 
@@ -22,9 +22,6 @@ BATCH_SIZE = 16
 IMAGE_SIZE = 224
 
 
-tf.reset_default_graph()
-
-
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
@@ -41,63 +38,66 @@ class _image_object:
         self.label = tf.Variable([], dtype = tf.int32)
 
 def read_and_decode(filename_queue):
-    reader = tf.TFRecordReader()
-    _, serialized_example = reader.read(filename_queue)
-    features = tf.parse_single_example(serialized_example, features = {
-        "image/encoded": tf.FixedLenFeature([], tf.string),
-        "image/height": tf.FixedLenFeature([], tf.int64),
-        "image/width": tf.FixedLenFeature([], tf.int64),
-        "image/filename": tf.FixedLenFeature([], tf.string),
-        "image/class/label": tf.FixedLenFeature([], tf.int64),})
-    image_encoded = features["image/encoded"]
-    image_raw = tf.image.decode_jpeg(image_encoded, channels=3)
-    image_object = _image_object()
-    image_object.image = tf.image.resize_image_with_crop_or_pad(image_raw, IMAGE_SIZE, IMAGE_SIZE)
-    image_object.height = features["image/height"]
-    image_object.width = features["image/width"]
-    image_object.filename = features["image/filename"]
-    image_object.label = tf.cast(features["image/class/label"], tf.int64)
+    with tf.name_scope('data_provider'):
+        reader = tf.TFRecordReader()
+        _, serialized_example = reader.read(filename_queue)
+        features = tf.parse_single_example(serialized_example, features = {
+            "image/encoded": tf.FixedLenFeature([], tf.string),
+            "image/height": tf.FixedLenFeature([], tf.int64),
+            "image/width": tf.FixedLenFeature([], tf.int64),
+            "image/filename": tf.FixedLenFeature([], tf.string),
+            "image/class/label": tf.FixedLenFeature([], tf.int64),})
+        image_encoded = features["image/encoded"]
+        image_raw = tf.image.decode_jpeg(image_encoded, channels=3)
+        image_object = _image_object()
+        image_object.image = tf.image.resize_image_with_crop_or_pad(image_raw, IMAGE_SIZE, IMAGE_SIZE)
+        image_object.height = features["image/height"]
+        image_object.width = features["image/width"]
+        image_object.filename = features["image/filename"]
+        image_object.label = tf.cast(features["image/class/label"], tf.int64)
     return image_object
 
 
 def flower_input(if_random = True, if_training = True):
-    if(if_training):
-        filenames = [os.path.join(DATA_DIR, "train.tfrecord")]
-    else:
-        filenames = [os.path.join(DATA_DIR, "test.tfrecord")]
+    with tf.name_scope('image_reader_and_preprocessor'):
+        if(if_training):
+            filenames = [os.path.join(DATA_DIR, "train.tfrecord")]
+        else:
+            filenames = [os.path.join(DATA_DIR, "test.tfrecord")]
 
-    for f in filenames:
-        if not tf.gfile.Exists(f):
-            raise ValueError("Failed to find file: " + f)
-    filename_queue = tf.train.string_input_producer(filenames)
-    image_object = read_and_decode(filename_queue)
-    image = tf.image.per_image_standardization(image_object.image)
-#    image = image_object.image
-#    image = tf.image.adjust_gamma(tf.cast(image_object.image, tf.float32), gamma=1, gain=1) # Scale image to (0, 1)
-    label = image_object.label
-    filename = image_object.filename
+        for f in filenames:
+            if not tf.gfile.Exists(f):
+                raise ValueError("Failed to find file: " + f)
+        filename_queue = tf.train.string_input_producer(filenames)
+        image_object = read_and_decode(filename_queue)
+        distorted_image = tf.image.random_flip_left_right(image_object.image)
+        image = tf.image.per_image_standardization(distorted_image)
+    #    image = tf.image.adjust_gamma(tf.cast(image_object.image, tf.float32), gamma=1, gain=1) # Scale image to (0, 1)
+        label = image_object.label
+        filename = image_object.filename
 
-    if(if_random):
-        min_fraction_of_examples_in_queue = 0.4
-        min_queue_examples = int(TRAINING_SET_SIZE * min_fraction_of_examples_in_queue)
-        print("Filling queue with %d images before starting to train. " "This will take a few minutes." % min_queue_examples)
-        num_preprocess_threads = 1
-        image_batch, label_batch, filename_batch = tf.train.shuffle_batch(
-            [image, label, filename],
-            batch_size = BATCH_SIZE,
-            num_threads = num_preprocess_threads,
-            capacity = min_queue_examples + 3 * BATCH_SIZE,
-            min_after_dequeue = min_queue_examples)
-        return image_batch, label_batch, filename_batch
-    else:
-        image_batch, label_batch, filename_batch = tf.train.batch(
-            [image, label, filename],
-            batch_size = BATCH_SIZE,
-            num_threads = 1)
-    image_batch = tf.reshape(image_batch, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
-    label_offset = -tf.ones([BATCH_SIZE], dtype=tf.int64, name="label_batch_offset")
-    label_batch = tf.one_hot(tf.add(label_batch, label_offset), depth=5, on_value=1.0, off_value=0.0)
-
+        if(if_random):
+            min_fraction_of_examples_in_queue = 0.4
+            min_queue_examples = int(TRAINING_SET_SIZE * min_fraction_of_examples_in_queue)
+            print("Filling queue with %d images before starting to train. " "This will take a few minutes." % min_queue_examples)
+            num_preprocess_threads = 1
+            image_batch, label_batch, filename_batch = tf.train.shuffle_batch(
+                [image, label, filename],
+                batch_size = BATCH_SIZE,
+                num_threads = num_preprocess_threads,
+                capacity = min_queue_examples + 3 * BATCH_SIZE,
+                min_after_dequeue = min_queue_examples)
+            image_batch = tf.reshape(image_batch, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
+            label_offset = -tf.ones([BATCH_SIZE], dtype=tf.int64, name="label_batch_offset")
+            label_batch = tf.one_hot(tf.add(label_batch, label_offset), depth=5, on_value=1.0, off_value=0.0)
+        else:
+            image_batch, label_batch, filename_batch = tf.train.batch(
+                [image, label, filename],
+                batch_size = BATCH_SIZE,
+                num_threads = 1)
+            image_batch = tf.reshape(image_batch, (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, 3))
+            label_offset = -tf.ones([BATCH_SIZE], dtype=tf.int64, name="label_batch_offset")
+            label_batch = tf.one_hot(tf.add(label_batch, label_offset), depth=5, on_value=1.0, off_value=0.0)
     return image_batch, label_batch, filename_batch
 
 
@@ -188,14 +188,13 @@ def flower_train():
     label_batch_placeholder = tf.placeholder(tf.float32, shape=[None, 5])
 
     logits = flower_inference(image_batch_placeholder)
+
 #    loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(labels=label_batch_one_hot, logits=logits_out))
     loss = tf.losses.mean_squared_error(labels=label_batch_placeholder, predictions=logits)
 
-
-
     saver = tf.train.Saver()
 
-    # create a summary for our cost and accuracy
+    # create a summary for training loss
     tf.summary.scalar('loss', loss)
 
     # merge all summaries into a single "operation" which we can execute in a session
@@ -204,11 +203,10 @@ def flower_train():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth=True
     with tf.Session(config=config) as sess:
-
         summary_writer = tf.summary.FileWriter("./logs", sess.graph)
-        tf.summary.scalar('loss', loss)
+
         sess.run(tf.global_variables_initializer())
-        saver.restore(sess, "./models/flower.ckpt")
+        #saver.restore(sess, "./models/flower.ckpt")
 
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord, sess = sess)
@@ -233,17 +231,20 @@ def flower_train():
         coord.request_stop()
         coord.join(threads)
         sess.close()
-
     return 0
 
+
 def main():
+    tf.reset_default_graph()
     flower_train()
+    #flower_eval()
+
 
 if __name__ == '__main__':
     main()
 
 
-#flower_eval()
+
 
 
 
